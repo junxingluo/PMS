d1be78fa632036324f2b5efcabc075b6ccb35cc9
Jun Luo <jun.luo@hotmail.ca>
2016-05-15 15:00:41 -0400

    src + pom.xml

---
+++ src/main/java/com/junapp/pms/HelloBean.java
@@ -0,0 +1,28 @@
+package com.junapp.pms;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionListener;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public ActionListener getAL() {
+		return null;
+	}
+}
